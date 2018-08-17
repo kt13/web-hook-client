cd0602599d395ca9d852ab55085f3f896192120b
Kevin <ketsang12@gmail.com>
2018-08-17 02:47:36 -0400

    image added

---
+++ src/components/aatasksToDo.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+
+//2 errors/bugs: adding a comment displays an error but it still adds it to the db
+// 2. map data array is showing different values sometimes. cant access marker.opening_hours.open_now 
+// cuz it doesn't exist sometimes
+//to do: accessibility, responsive design, tests, comments, admin user, css
--- src/components/aller-search-form.js
+++ src/components/aller-search-form.js
@@ -6,9 +6,6 @@ import './aller-search.css';
 import {toggleFoodsList} from '../actions/foods';
 import {fetchFoods} from '../actions/foods';
 
-//2 errors/bugs: adding a comment displays an error but it still adds it to the db
-// 2. map data array is showing different values sometimes. cant access marker.opening_hours.open_now 
-// cuz it doesn't exist sometimes
 export class AllergyForm extends React.Component {
  
   // onSubmit (event){
@@ -29,12 +26,15 @@ export class AllergyForm extends React.Component {
       <div id="AllergySearch" className="tabcontent" id="defaultOpen">
         <div className='foodSearch'>
           <h2>Enter in a Food</h2>
+          <img className="allergyImg" src="https://foodsafetytrainingcertification.com/wp-content/uploads/2017/05/foodallergies.png" 
+            alt="Various Food Categories of Food Allergies"/>
           <p>This database is open-source and is maintained by the public.</p>
 
           <p>Please note that this website is not a substitute for medical advice. If you are experiencing 
             symptoms of an allergic reaction such as hives, vomiting, wheezing, 
             swelling of the tongue, or trouble breathing due to possible anaphylaxis, 
-            please seek emergency care immediately. For more information on food allergies, <a href="https://acaai.org/allergies/types/food-allergy">go here</a>.</p>
+            please seek emergency care immediately.</p>
+          <span>For more information on food allergies, <a href="https://acaai.org/allergies/types/food-allergy">go here</a>.</span>
 
           <div className='allergyForm'>
             <form onSubmit={e =>  
