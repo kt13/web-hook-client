b6e743861330e7c44048a2e476983f24ab9ffba4
Kevin <ketsang12@gmail.com>
2018-08-24 19:07:29 -0400

    small modularization changes

--- src/components/post-food.js
+++ src/components/post-food.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './post-food.css';
 import { connect } from 'react-redux';
 import {withRouter} from 'react-router-dom';
-import {postNewFood} from '../actions/foods';
+import PostFoodForm from './post-food-form';
 
 export class PostFood extends React.Component{
 
@@ -11,8 +11,9 @@ export class PostFood extends React.Component{
       setTimeout(() => {this.props.history.push('/login');}, 15000);
       return(
         <div>
-          <p style={{color: 'rgb(172, 4, 4)'}}>You need to be logged in to contribute a new listing to the database.
-            Redirecting you to login in 15 seconds...
+          <p style={{color: 'rgb(172, 4, 4)'}}>You need to be logged in to 
+          contribute a new listing to the database.
+          Redirecting you to login in 15 seconds...
           </p>
         </div>);
     }
@@ -33,46 +34,8 @@ export class PostFood extends React.Component{
           can not be created.</p>
 
           {this.credentials()}
-
-          <form onSubmit={e => {
-            e.preventDefault();
-            console.log(e.target.elements.ingredients.value,
-              e.target.ingredients.value.split(' '),  '---');
-            const el = e.target.elements;
-            this.props.dispatch(postNewFood(
-              el.foodName.value, 
-              el.ingredients.value.split(' ')));}}>
-          
-            <div className='input'>
-              <label>Name</label><br />
-              <input type='text' 
-                className='name' 
-                name='foodName'
-                aria-labelledby="Add Food Name"></input>
-          
-            </div>
-          
-            <div className='input'>
-              <label>Ingredients</label> <br/>
-              <textarea 
-                type='text' 
-                name='ingredients' 
-                className='postText' 
-                aria-labelledby="Add Ingredients"></textarea>
-              <br />
-              <label className='post-footnote'>Separate ingredients with a space.</label>
-         
-            </div>
-         
-            <input 
-              type="submit" 
-              id="postButton" 
-              className="postButton" 
-              name="submit" 
-              value="Create"/>
-
-          </form>
-     
+          <PostFoodForm />
+      
         </div>
     
       </div>
